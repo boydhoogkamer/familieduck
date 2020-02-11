4a3961eb97e1df1dd12e57d1ea9d12fc2df29d86
boydhoogkamer <43574994+boydhoogkamer@users.noreply.github.com>
2020-02-11 09:56:00 +0100

    UX aanpassingen
    Een paar kleine aanpassing aan de UX.

--- familieDuck-master/script.js
+++ familieDuck-master/script.js
@@ -10,30 +10,26 @@ for(let i=0; i<plaatjes.length; i++) {
     plaatjes[i].addEventListener('mouseenter', vulKopAan);
     plaatjes[i].addEventListener('mouseleave', resetKop);
     plaatjes[i].addEventListener('click', onClick);
-    plaatjes[i].addEventListener('dblclick', dblClick);
     likeButton[i].addEventListener('click', onClickP);
-    // hier kun je nog 2 events aan toevoegen: click en dblclick
 }
 
 function vulKopAan() {
     document.getElementById('duck').innerText = ": "+this.title;
     document.getElementById('kop').style.color  = this.dataset.kleur;
+    this.style.border = 'solid 7px ' + this.dataset.kleur;
+    document.getElementById('titel').innerHTML = this.title;
+    document.getElementById('titel').style.color = this.dataset.kleur;
+    document.getElementById('informatie').innerHTML = this.dataset.info;
+    document.getElementById('info').style.border = 'solid 7px ' + this.dataset.kleur;
 }
 
 function resetKop() {
     document.getElementById('duck').innerText = "";
     document.getElementById('kop').style.color  = 'red';
+    this.style.border = 'none';
 }
 
 function onClick() {
-    this.style.border = 'solid 3px ' + this.dataset.kleur;
-    document.getElementById('titel').innerHTML = this.title;
-    document.getElementById('titel').style.color = this.dataset.kleur;
-    document.getElementById('informatie').innerHTML = this.dataset.info;
-    document.getElementById('info').style.border = 'solid 3px ' + this.dataset.kleur;
-}
-
-function dblClick() {
     this.dataset.clicks++
     this.nextSibling.innerHTML = this.dataset.clicks;
 
